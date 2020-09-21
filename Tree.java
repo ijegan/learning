138bb05ec2603a99573470a07b2c254961ff6ded
ijegan <67261030+ijegan@users.noreply.github.com>
2020-09-22 00:33:14 +0530

    Update Tree.java

--- Tree.java
+++ Tree.java
@@ -1,9 +1,10 @@
-package com.demo.kafka;
+package com.sample;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Tree {
 
@@ -21,14 +22,10 @@ public class Tree {
 				"ITERGO_D_C2_T2_Topic", "ITERGO_D_C3_T1_Topic", "ITERGO_D_C3_T2_Topic", "ITERGO_D_C3_T3_Topic",
 				"ITERGO_D_C3_T4_Topic", "ITERGO_D_C3_T1_X1_Topic01", "ITERGO_D_C3_T1_X1_Topic02" };
 
-//		String[] subscopes = new String[] { "ITERGO_D", "ITERGO_D_A", "ITERGO_D_C", "ITERGO_D_A1", "ITERGO_D_A2",
-//				"ITERGO_D_A3", "ITERGO_D_B1", "ITERGO_D_B2", "ITERGO_D_B3", "ITERGO_D_C1", "ITERGO_D_C2"
-//
-//		};
-
 		String[] subscopes = new String[] { "ITERGO_D_A3", "ITERGO_D_C2", "ITERGO_D", "ITERGO_D_A", "ITERGO_D_C",
 				"ITERGO_D_B3", "ITERGO_D_A1", "ITERGO_D_A2", "ITERGO_D_B1", "ITERGO_D_B2", "ITERGO_D_A1_T1",
-				"ITERGO_D_A1_T2", "ITERGO_D_B1_T1", "ITERGO_D_B1_T2", "ITERGO_D_C1_T1", "ITERGO_D_C1_T2", "ITERGO_D_C1",
+				"ITERGO_D_A1_T2", "ITERGO_D_B1_T1", "ITERGO_D_B1_T2", "ITERGO_D_C1_T1", "ITERGO_D_C1_T2", "ITERGO_D_C1"
+//				"ITERGO_D_B"
 
 		};
 
@@ -38,12 +35,11 @@ public class Tree {
 		List<String> subscopeList = new ArrayList<>();
 		subscopeList.addAll(Arrays.asList(subscopes));
 
-//		Collections.sort(subscopeList);
-
 		// sort in descending order of length
 		subscopeList.sort((s1, s2) -> Math.abs(s1.length()) - Math.abs(s2.length()));
 
-		subscopeList.forEach(value -> System.out.println(value));
+//		subscopeList.forEach(
+//				value -> System.out.println(value));
 
 		Tree tree = new Tree();
 
@@ -51,16 +47,19 @@ public class Tree {
 			tree.add(scope);
 		}
 
-//		traverseInOrder(root);
+		System.out.println("\n");
+		System.out.println("\n");
 
-		// pointing to root node
-//		containsNode("ITERGO_D_A1_T1");
-//		for (String topic : topicList) {
-//			addTopic(topic);
-//		}
+		// adding topics to scope
+		for (String topic : topicList) {
+			tree.addTopic(topic);
+		}
 
 		System.out.println("\n");
 		System.out.println("\n");
+
+		System.out.println(root.value + " " + root.topics);
+		traverseInOrder(root);
 
 //		traverseInOrderScope(root);
 
@@ -70,66 +69,72 @@ public class Tree {
 		currentNode = addRecursive(currentNode, value);
 	}
 
+	public void addTopic(String value) {
+		currentNode = addTopicSubscope(root, value);
+	}
+
 	private Node addRecursive(Node current, String value) {
 		// root node
 		if (current == null) {
 			root = new Node(value);
-			System.out.println("Root Node added " + value + " , level " + root.level);
+			System.out.println("root added | " + value + "|  level " + root.level);
 			return root;
 		}
-
-		boolean insertionFlag = false;
 
 		if (value.startsWith(current.value)) {
 			// ITERGO_D , ITERGO_D_B , ITERGO_D_C , ITERGO_D_X, ITERGO_D_Y, ITERGO_D_B ,
 			// ITERGO_D_B_B1 , ITERGO_D_B_B2 , ITERGO_D_B_B2_B3
 
-			for (Node node : current.nodes) {
-				if (value.startsWith(node.value)) {
-					if (!insertionFlag)
-						addRecursive(node, value);
-				}
-			}
+			// ITERGO_D , ITERGO_D_A , ITERGO_D_C , ITERGO_D_A3, ITERGO_D_C2, ITERGO_D_B3 ,
+			// ITERGO_D_A1 , ITERGO_D_A2
 
-			if (insertionFlag)
-				return current;
-			// next level
-			if (current.nodes.isEmpty()) {
-				Node next = new Node(value);
-				next.level = current.level + 1;
-				current.nodes.add(next);
-				System.out.println("node added " + value + " , level " + next.level);
-				insertionFlag = true;
-				return next;
-			}
+			current = navigateToChildNode(current, value);
 
-			// same level ??
-			if (!current.nodes.isEmpty()) {
-				Node next = new Node(value);
-				next.level = current.level + 1;
-				current.nodes.add(next);
-				System.out.println("node added in same level " + value + " level " + next.level);
-				insertionFlag = true;
-				current = next;
-				return next;
-			}
+			Node next = new Node(value);
+			next.level = current.level + 1;
+
+			current.nodes.add(next);
+			System.out.println("node added | " + value + "|  level " + next.level + " | parent " + current.value);
+			return current;
+
 		}
 		// start from root
 		else {
-			current = root;
-			addRecursive(current, value);
+//			current = root;
+			addRecursive(root, value);
 		}
 
-//	if(value.compareTo(current.value)<0)
-//	{
-//		current.left = addRecursive(current.left, value);
-//	}else if(value.compareTo(current.value)>0)
-//	{
-//		current.right = addRecursive(current.right, value);
-//	}else
-//	{
-		// value already exists
 		return current;
+	}
+
+	private Node addTopicSubscope(Node current, String topic) {
+
+		if (topic.startsWith(current.value)) {
+
+			current = navigateToChildNode(current, topic);
+
+			current.topics.add(topic);
+			System.out.println("topic added | " + topic + "|  level " + current.level + " | scope " + current.value);
+			return current;
+
+		}
+		// start from root
+		else {
+			addRecursive(root, topic);
+		}
+
+		return current;
+
+	}
+
+	private Node navigateToChildNode(Node current, String value) {
+		for (Node node : current.nodes) {
+			if (value.startsWith(node.value)) {
+				current = navigateToChildNode(node, value);
+			}
+		}
+		return current;
+
 	}
 
 	private static boolean containsNodeRecursive(Node current, String value) {
@@ -148,49 +153,24 @@ public class Tree {
 		return containsNodeRecursive(root, value);
 	}
 
-	private static boolean addTopicsSubscopes(Node current, String value) {
-		if (current == null) {
-//			System.out.println("Not able to add");
-			Node position = findIkepNamespaceNode(root, "ITERGO_D");
-			position.topics.add(value);
-			return false;
-		}
-		if (value.startsWith(current.value)) {
-			current.topics.add(value);
-			return true;
-		}
-
-		System.out.println(value + " ~~ " + current.value + " ~~" + value.compareTo(current.value));
-
-//		return (value.compareTo(current.value) < 0) ? addTopicsSubscopes(current.left, value)
-//				: addTopicsSubscopes(current.right, value);
-		return false;
-	}
-
-	public static boolean addTopic(String value) {
-		return addTopicsSubscopes(root, value);
-	}
-
-	private static Node findIkepNamespaceNode(Node current, String value) {
-//		if (current == null) {
-//			return current;
-//		}
-		if (value == current.value) {
-			return current;
-		}
-//		return (value.compareTo(current.value) < 0) ? findIkepNamespaceNode(current.left, value)
-//				: findIkepNamespaceNode(current.right, value);
-
-		return current;
-	}
-
-//	public static void traverseInOrder(Node node) {
+	public static void traverseInOrder(Node current) {
 //		if (node != null) {
 //			traverseInOrder(node.left);
 //			System.out.println(" " + node.value);
 //			traverseInOrder(node.right);
 //		}
-//	}
+//		System.out.println(root.value + " " + root.topics);
+
+		if (!current.nodes.isEmpty()) {
+			for (Node node : current.nodes) {
+				System.out.println(node.value + " " + node.topics);
+				if (!node.nodes.isEmpty())
+					traverseInOrder(node);
+			}
+
+		}
+
+	}
 //
 //	public static void traverseInOrderScope(Node node) {
 //		if (node != null) {
@@ -204,12 +184,12 @@ public class Tree {
 class Node {
 	String value;
 	int level;
-	List<Node> nodes;
+	Set<Node> nodes;
 	List<String> topics;
 
 	Node(String value) {
 		this.value = value;
-		nodes = new ArrayList<>();
+		nodes = new HashSet<>();
 		topics = new ArrayList<>();
 		if (value.equals("ITERGO_D"))
 			level = 0;
