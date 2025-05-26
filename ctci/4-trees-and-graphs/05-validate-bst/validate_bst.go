dbabcf6b4506a921788d24d4fdc5f0098a79d769
ijegan <i.jegan@gmail.com>
2025-05-26 12:52:36 +0200

    4.5 validate-bst

---
+++ ctci/4-trees-and-graphs/05-validate-bst/validate_bst.go
@@ -0,0 +1,97 @@
+package graph
+
+func ValidateBst(root *Node) bool {
+	arr := InOrder(root)
+	for i, j := 0, 1; j < len(arr); i, j = i+1, j+1 {
+		if arr[i] > arr[j] {
+			return false
+		}
+	}
+	return true
+}
+
+type Node struct {
+	data  int
+	left  *Node
+	right *Node
+}
+
+func BuildBst(arr []int) *Node {
+	var root *Node
+	for _, v := range arr {
+		if root == nil {
+			root = &Node{data: v, left: nil, right: nil}
+			continue
+		}
+		createNode(root, v)
+	}
+	return root
+}
+
+func Search(root *Node, value int) *Node {
+	for ptr := root; ptr != nil; {
+		if value == ptr.data {
+			return ptr
+		} else if value < ptr.data {
+			ptr = ptr.left
+		} else if value > ptr.data {
+			ptr = ptr.right
+		}
+	}
+	return nil
+}
+
+// ptr point the node found using search and insert a new value
+func Insert(ptr *Node, value int) {
+	if ptr == nil {
+		return
+	}
+
+	newNode := &Node{data: value, left: nil, right: nil}
+	// insert newNode if the child is empty
+	if value < ptr.data {
+		if ptr.left == nil {
+			ptr.left = newNode
+		}
+	} else if value > ptr.data {
+		if ptr.right == nil {
+			ptr.right = newNode
+		}
+	}
+}
+
+func createNode(root *Node, value int) {
+
+	for ptr := root; ptr != nil; {
+		if value == ptr.data {
+			return
+		} else {
+			newNode := &Node{data: value, left: nil, right: nil}
+
+			if value < ptr.data {
+				if ptr.left == nil {
+					ptr.left = newNode
+					return
+				}
+				ptr = ptr.left
+			} else if value > ptr.data {
+				if ptr.right == nil {
+					ptr.right = newNode
+					return
+				}
+				ptr = ptr.right
+			}
+		}
+	}
+}
+
+func InOrder(ptr *Node) []int {
+	var arr []int
+	if ptr != nil {
+		arr = append(arr, InOrder(ptr.left)...)
+		arr = append(arr, ptr.data)
+		arr = append(arr, InOrder(ptr.right)...)
+		return arr
+	}
+	return arr
+}
---
+++ ctci/4-trees-and-graphs/05-validate-bst/validate_bst_test.go
@@ -0,0 +1,68 @@
+package graph
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestValidateBst(t *testing.T) {
+
+	tests := []struct {
+		i, o []int
+	}{
+		{i: []int{15, 12, 11, 13, 14, 18, 16, 17, 19, 20}, o: []int{11, 12, 13, 14, 15, 16, 17, 18, 19, 20}},
+		{i: []int{3, 4, 1, 5, 6, 2}, o: []int{1, 2, 3, 4, 5, 6}},
+	}
+
+	for _, test := range tests {
+
+		var root *Node
+		root = BuildBst(test.i)
+
+		got := InOrder(root)
+		fmt.Println(got)
+		want := test.o
+
+		// validate InOrder display
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Dispay(%v) =  %v, want %v", test.i, got, want)
+		}
+
+		flag := ValidateBst(root)
+		if !flag {
+			t.Errorf("Dispay(%v) =  %v, | Not a binary search tree", test.i, got)
+		}
+
+	}
+
+	// Search and insert
+	var root *Node
+	root = BuildBst(tests[0].i)
+
+	ptr := Search(root, 14)
+	Insert(ptr, 25)
+	ptr = Search(root, 11)
+	Insert(ptr, 7)
+
+	display := InOrder(root)
+	fmt.Println(display)
+
+	flag := ValidateBst(root)
+	fmt.Println("flag: ", flag)
+	if !flag {
+		fmt.Println("Dispay([ 7,11..20,25 ]) | Not a binary search tree")
+	}
+
+	// Test another input
+	root = BuildBst(tests[0].i)
+	ptr = Search(root, 11)
+	Insert(ptr, 7)
+	display = InOrder(root)
+
+	fmt.Println(display)
+	flag = ValidateBst(root)
+	if !flag {
+		t.Errorf("Dispay(%v) =  %v, | Not a binary search tree", "7,11..20", display)
+	}
+}
